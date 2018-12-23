99d3d884f110e59a5eba95dbfbf3c1a8475e05a6
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-23 23:33:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@
 def oxford_comma(array)
   #if array.count == 1 
     #array.join 
-  if array.length == 2 
+  if array.size == 2 
     array.join(" and ")
   elsif array.count > 2
     array[-1].insert(0, "and ")
