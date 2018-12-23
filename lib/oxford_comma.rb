d7d6e3fd0a277b046e7b6f7b04b1ed40c07f8044
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-23 23:27:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#   new_array = array.join(",")
+#   new_array.insert(-1, "and")
+# end
 
-end
+def oxford_comma(array)
+  #if array.count == 1 
+    #array.join 
+  if array.count == 2 
+    array.join(" and ")
+  elsif array.count > 2
+    array[-1].insert(0, "and ")
+    #and_array = array.join(", ")
+    #and_array.insert(-2, "and #{and_array[-1]}")
+  #else
+    #and_array = array.join(", ")
+    #and_array.insert(-2, "and #{and_array[-1]}")
+  end
+  array.join(", ")
+end 
